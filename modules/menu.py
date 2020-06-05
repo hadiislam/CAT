8023def4533e65ed8ea4fa6224897756b6648ef3
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-06 04:58:36 +0600

    Update menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -121,25 +121,7 @@ class main:
               if os.path.exists(system.bin+"/CAT") and os.path.exists(system.conf_dir+"/CAT"):
                 os.system("clear")
                 logo.updated()
-                cmd=input("\033[m[\033[32m ? \033[m]\033[32m Choose : \033[m")
-
-                else:
-                  os.system("rm -rf "+system.bin+"/CAT")
-                  os.system("rm -rf "+system.conf_dir+"/CAT")
-                  os.system("cd")
-                  os.system("git clone https://github.com/hadiislam/CAT")
-                  os.system("cd CAT")
-                  os.system("chmod +x *")
-                  os.system("mkdir "+system.conf_dir+"/CAT")
-                  os.system("cp -r modules core CAT.py "+system.conf_dir+"/CAT")
-                  os.system("cp -r core/CAT "+system.bin)
-                  os.system("cp -r core/CAT "+system.bin)
-                  os.system("chmod +x "+system.bin+"/CAT")
-                  os.system("cd .. && rm -rf CAT")
-                  os.system("clear")
-                  logo.updated()
-                  cmd=inputut("Please Restart CyberCat")
-                  
+                cmd=input("\033[m[\033[32m ? \033[m]\033[32m Choose : \033[m]
               else:
                 os.system("clear")
                 logo.update_error()
