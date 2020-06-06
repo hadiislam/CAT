dd6dcc07a0b5e65e44ed0b495fa21b0d1d5d343e
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-06 07:03:21 +0600

    Update menu.py

--- modules/menu.py
+++ modules/menu.py
@@ -118,7 +118,7 @@ class main:
               os.system(system.sudo+" git clone https://github.com/hadiislam/CAT.git "+system.home+"/")
             if os.path.exists(system.home+"/CAT/install.sh"):
               os.system("cd "+system.home+"/CAT && "+system.sudo+" sh install.sh")
-              if os.path.exists(system.bin+"/CAT") and os.path.exists(system.conf_dir+"/CAT"):
+              if os.path.exists(system.bin+"/CAT"):
                 os.system("cd")
                 os.system("git clone https://github.com/hadiislam/CAT")
                 os.system("mkdir "+system.conf_dir+"/CAT")
